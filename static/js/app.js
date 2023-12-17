66d41c0da9606a3ca0ba8c0eb04dd164f7ce6612
aryavardhanjain <jainaryan418@gmail.com>
2023-12-17 10:34:53 +0530

    Changes to go live

---
+++ static/js/app.js
@@ -0,0 +1,8 @@
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.utils.toArray("span").forEach((span) => {
+        ScrollTrigger.create({
+            trigger: span,
+            start: "top center",
+            onEnter: () => span.classList.add("active"),
+        });
+    });
